1b3e759d7df206902d26aa01722c1a817c25d1c8
Nikita <nik.vakula@bk.ru>
2022-08-04 12:31:12 +0300

    fix cors settings

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001, https://nikitavakula-auth-app.vercel.app/'],
+    origin: ['http://localhost:3001', 'https://nikitavakula-auth-app.vercel.app'],
     methods: '*',
     credentials: true,
   });
